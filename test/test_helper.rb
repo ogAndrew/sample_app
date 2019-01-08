4e44f4c8e244b924b8b08585999bbcdde3f78168
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-08 18:33:15 -0500

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,1 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require "minitest/reporters"
-Minitest::Reporters.use!
-
-
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-  include ApplicationHelper
-
-  # Returns true if a test user is logged in.
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
-end
+# This file needs to be changed.  Code was lost during tutorial 
